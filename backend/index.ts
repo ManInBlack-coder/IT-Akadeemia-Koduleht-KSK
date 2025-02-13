c07f2833ea85c72a099567ee9a64ec2eb9b751bb
gitTanzj <kalle.riit@gmail.com>
2025-02-13 12:48:41 +0200

    redid the directory

---
+++ backend/index.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response } from 'express';
+import 'dotenv/config'
+const PORT = process.env.PORT || 3000
+
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+    res.send("Hello world!")
+})
+
+
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`)
+})
