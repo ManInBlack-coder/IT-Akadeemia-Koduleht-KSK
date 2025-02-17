4d9ee3ab72825d228d8b6028a668d3fe2ab9c561
gitTanzj <kalle.riit@gmail.com>
2025-02-17 10:29:15 +0200

    routes setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,31 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
+
+import Avaleht from './components/Avaleht/Avaleht';
+import Erialad from './components/Erialad/Erialad';
+import Praktika from './components/Praktika/Praktika';
+import Tunniplaan from './components/Tunniplaan/Tunniplaan';
+import Voistlused from './components/Voistlused/Voistlused';
 
 function App() {
 
   return (
-    <>  
-      <h2 className="text-3xl">IT Akadeemia koduleht</h2>
-      <p>Website coming soon</p>
-    </>
+    <BrowserRouter>
+      <div className="flex w-full min-h-screen justify-center">
+        <Header/>
+        <Routes>
+          <Route path="/" element={<Avaleht/>}/>
+          <Route path="/erialad" element={<Erialad/>}/>
+          <Route path="/praktika" element={<Praktika/>}/>
+          <Route path="/tunniplaan" element={<Tunniplaan/>}/>
+          <Route path="/voistlused" element={<Voistlused/>}/>
+        </Routes>
+        <Footer/>
+      </div>
+    </BrowserRouter>
   )
 }
 
