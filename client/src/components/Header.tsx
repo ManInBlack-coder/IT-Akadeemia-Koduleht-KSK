3a1e7ccb085ff301763e548a6532705178f3633f
gitTanzj <kalle.riit@gmail.com>
2025-02-17 10:46:15 +0200

    header added

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,7 +1,30 @@
 import React from 'react'
+import headerLogo from '../assets/logo.svg'
+import { Link } from 'react-router-dom'
 
 export const Header = () => {
   return (
-    <div>Header</div>
+    <div className="w-full bg-black p-4 flex flex-col md:flex-row justify-center items-center gap-12">
+        <Link to="/">
+            <img src={headerLogo}/>
+        </Link>
+        <div className="text-white flex gap-12 h-full items-center">
+            <Link to="/erialad">
+                <p>Erialad</p>
+            </Link>
+            <Link to="/praktika">
+                <p>Praktika</p>
+            </Link>
+            <Link to="/tunniplaan">
+                Tunniplaan
+            </Link>
+            <Link to="/konsultatsioonid">
+                Konsultatsioonid
+            </Link>
+            <Link to="/voistlused">
+                Võistlused
+            </Link>
+        </div>
+    </div>
   )
 }
