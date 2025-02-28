df208b5098455cebf02b6096c60c068973afe5c9
gitTanzj <kalle.riit@gmail.com>
2025-02-28 20:01:16 +0200

    to desktop

--- client/src/pages/Praktika/Praktika.tsx
+++ client/src/pages/Praktika/Praktika.tsx
@@ -1,8 +1,14 @@
+import praktikaBackground1 from '../../assets/praktika/praktikaBackground1.png'
+import './Praktika.css'
 
 const Praktika = () => {
   return (
-    <div>
-      
+    <div className="flex flex-col w-full min-h-screen justify-start items-center">
+      <div className={`praktika-bg-one flex w-full justify-center h-96`}>
+        <div className="z-1">
+          <h1 className="text-white">PRAKTIKAVÕIMALUSED IT AKADEEMIAS</h1>
+        </div>
+      </div>
     </div>
   )
 }
