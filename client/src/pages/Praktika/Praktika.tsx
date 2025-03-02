cfd1c57c9fbe9e521413928f21e31395070ac539
gitTanzj <kalle.riit@gmail.com>
2025-03-02 13:02:21 +0200

    feat:praktika section complete

--- client/src/pages/Praktika/Praktika.tsx
+++ client/src/pages/Praktika/Praktika.tsx
@@ -3,6 +3,7 @@ import { Button } from '../../components/Button'
 import vocoLogo from '../../assets/logos/voco muster RGB-03 2.svg'
 import calendarIcon from '../../assets/icons/calendar.svg'
 import arrowRight from '../../assets/icons/arrow_right.svg'
+import documentIcon from '../../assets/icons/document_icon.svg'
 import { PraktikaJourneryComponent } from './PraktikaJourneryComponent'
 
 
@@ -11,6 +12,9 @@ import bitwebLogo from '../../assets/logos/Bitweb logo 1.png';
 import diaraInfraLogo from '../../assets/logos/Diara Infra logo 1.png';
 import g4sLogo from '../../assets/logos/G4S_Eesti_logo 1.png';
 import kliinikumLogo from '../../assets/logos/Tartu ülikooli kliinikumi logo 1.png';
+
+import erasmusLogo from '../../assets/praktika/ErasmusLogo.png'
+import praktikaDocumentsImage from '../../assets/praktika/praktikaMaterjalidImage.png'
 
 type PraktikaJourneyComponentData = {
   title: string;
@@ -22,6 +26,11 @@ type PraktikaPartner = {
   name: string;
   desc: string;
   logo: string;
+}
+
+type PraktikaLink = {
+  text: string;
+  link: string;
 }
 
 const Praktika = () => {
@@ -118,19 +127,42 @@ const Praktika = () => {
     }
   ]
 
+  const praktikaLinks: PraktikaLink[] = [
+    {
+      text: "CV näidis",
+      link: "/"
+    },
+    {
+      text: "Motivatsioonikirja juhend",
+      link: "/"
+    },
+    {
+      text: "Praktikakohad",
+      link: "/"
+    },
+    {
+      text: "Praktikaprotsess",
+      link: "/"
+    },
+    {
+      text: "Praktika tingimused",
+      link: "/"
+    }
+  ]
+
   return (
-    <div className="flex flex-col w-full min-h-screen justify-start items-center">
+    <div className="flex flex-col w-full min-h-screen justify-start items-center md:gap-4">
       <div className={`praktika-bg-one flex w-full justify-center min-h-96 px-4`}>
         <div className="z-1 flex text-white font-semibold justify-center items-center flex-col text-center gap-8 md:gap-12 py-8">
-          <h1 className="text-heading1-bold text-2xl md:text-[40px]">PRAKTIKAVÕIMALUSED IT AKADEEMIAS</h1>
-          <h2 className="text-heading3-medium text-lg md:text-xl">Kujunda oma tulevik praktiliste kogemustega Eestis ja välismaal</h2>
+          <h1 className="text-heading3-bold md:text-heading1-bold text-2xl md:text-[40px]">PRAKTIKAVÕIMALUSED IT AKADEEMIAS</h1>
+          <h2 className="text-base-medium md:text-heading3-medium text-lg md:text-xl">Kujunda oma tulevik praktiliste kogemustega Eestis ja välismaal</h2>
         </div>
       </div>
       <div className="w-full min-h-64 flex flex-col justify-center items-center gap-8 md:gap-12 py-12 md:py-24 px-4">
         <div className="flex flex-col justify-center items-center w-full md:w-2/3 text-center">
-          <p className="text-heading5-bold">Oleme rahvusvahelisusele avatud haridusasutus ja aktiivne partner koostööprojektides.</p>
+          <p className="text-base-bold md:text-[20px]">Oleme rahvusvahelisusele avatud haridusasutus ja aktiivne partner koostööprojektides.</p>
           <br/>
-          <p className="text-heading5-medium">
+          <p className="text-base-medium md:text-[20px]">
             Alates 2016. aastast liitusime õpirände hartaga ning 2021. aastal omistati Tartu Rakenduslikule Kolledžile Erasmus+ õpirände akrediteering, mis tähendab, et perioodil 2021 – 2027 saab kool lihtsustatud korras toetust uute õpirännete rahastamiseks.
           </p>
         </div>
@@ -138,18 +170,19 @@ const Praktika = () => {
       </div>
       <div className="flex w-full justify-center items-center min-h-96 bg-vocogray py-8 md:py-12 px-4">
         <div className="flex flex-col md:flex-row w-full md:w-5/6 justify-between items-center gap-8">
-          <div className="w-full md:w-1/2 flex flex-col justify-start items-start gap-8">
-            <div className="w-full md:w-2/3 flex flex-col justify-start items-start gap-4">
-              <h2 className="text-heading2-bold">Praktikaperioodid</h2>
-              <p className="text-large-medium">Vähemalt kaks nädalat enne praktika algust valmistab kolledžipoolne praktikajuhendaja ette praktika dokumendid (praktikalepingu ja lisad) ja selgitab nende täitmist.</p>
+          <div className="w-full md:w-1/2 flex flex-col justify-start items-center md:items-start gap-8">
+            <div className="w-full md:w-2/3 flex flex-col justify-start items-center md:items-start gap-4">
+              <h2 className="text-heading5-bold md:text-heading2-bold">Praktikaperioodid</h2>
+              <p className="text-base-medium md:text-large-medium text-center md:text-left">Vähemalt kaks nädalat enne praktika algust valmistab kolledžipoolne praktikajuhendaja ette praktika dokumendid (praktikalepingu ja lisad) ja selgitab nende täitmist.</p>
             </div>
             <div className="flex gap-2 items-center justify-center hover:cursor-pointer">
-              <img src={calendarIcon} alt="calendar"/>
+              <img src={calendarIcon} alt="calendar" className="hidden md:block"/>
               <p className="text-base-bold text-itkool hover:underline">Praktika kuupäevad ja kestus</p>
               <img src={arrowRight} alt="arrow"/>
             </div>
+            <Button buttonText="Vaata juhendeid" color='black'/>
           </div>
-          <div className="w-full md:w-1/2 flex justify-center md:justify-end items-center">
+          <div className="w-full md:w-1/2 hidden md:flex justify-center md:justify-end items-center">
             <img src={vocoLogo} alt="voco logo" className="w-3/4 md:w-auto"/>
           </div>
         </div>
@@ -183,12 +216,12 @@ const Praktika = () => {
           </div>
         </div>
       </div>
-      <div className="flex w-full justify-center items-center min-h-96 py-8 md:py-12 px-4">
-        <div className="flex flex-col w-5/6 justify-center items-start gap-8">
+      <div className="flex w-full justify-center items-center min-h-96 py-12 px-4">
+        <div className="flex flex-col w-5/6 justify-center items-center md:items-start gap-8">
           <h2 className="text-heading2-bold">Meie praktikapartnerid</h2>
-          <div className="grid grid-cols-1 md:grid-cols-2 w-full gap-8">
+          <div className="grid grid-cols-1 md:grid-cols-2 w-full gap-24 md:gap-8">
             {praktikaPartners.map((partner) => (
-              <div className="flex flex-col items-start justify-start gap-4">
+              <div className="flex flex-col items-center md:items-start justify-start gap-4">
                 <h3 className="text-heading5-bold text-itkool">{partner.name}</h3>
                 <p className="text-large-medium">{partner.desc}</p>
                 <img src={partner.logo} height={40}/>
@@ -196,6 +229,47 @@ const Praktika = () => {
             ))}
           </div>
         </div>  
+      </div>
+      <div className="praktika-bg-two flex w-full justify-center items-center min-h-96 py-12 md:py-16 px-4">
+        <div className="flex relative w-5/6 flex-col justify-center items-center md:items-start gap-8">
+          <h2 className="text-heading2-bold text-center md:text-left">Võimalused välispraktikaks</h2>
+          <div>
+            <h3 className="text-heading5-bold text-center md:text-left">Mida teha, et välismaale praktikale või õppima minna?</h3>
+            <br/>            
+            <p className="text-base-medium text-center md:text-left">
+              Kõigepealt palume Sul oma huvist märku anda, et teaksime õpirändeid kavandada. Selleks mõtle läbi miks, kuna ja millises riigis sooviksid oma praktikat teha.  Tartu Rakenduslikul Kolledžil on koostööpartnerid peaaegu kõigis Euroopa riikides. Vaata kaarti SIIT.
+              Soovitame juba ette välispraktika planeerida oma praktikaperioodiga samale ajale. Ja loomulikult kõige tähtsam – mõtle, mis on Su praktika eesmärk ja mida soovid välispraktika raames teha!
+              <br/>
+              <br/>
+              Seejärel võtab meie rahvusvaheliste suhete koordinaator Sinuga ühendust, et täpsustada välispraktika tingimusi.  Ta  otsib vastuvõtvad partnerid õpirännete korraldamiseks ja kui koos vastuvõtjaga on leitud sobiv praktikakoht ja aeg, siis tuleb täita ametlik välispraktika avaldus ning muud dokumendid. Enamasti korraldatakse ka enne praktikale minekut online kohtumine ja mõistagi on soovitav leida aega, et vastuvõtva riigi kultuuri ja keelega tutvuda.
+              <br/>
+              <br/>
+              <br/>
+              Loe õpilaste ja õpetajate õpirände blogisid:
+            </p>
+          </div>
+          <Button buttonText='VOCOMOBILTY' color='blue'/>
+          <img className='-z-10 absolute bottom-0 right-10' src={erasmusLogo}/>
+        </div>
+      </div>
+      <div className="flex w-full justify-center items-center min-h-96 py-8 md:py-12 px-4">
+        <div className="flex flex-col md:flex-row w-5/6 justify-center items-center gap-8">
+          <div className="hidden md:flex w-1/2 justify-center items-center">
+            <img src={praktikaDocumentsImage} className="w-full max-w-md"/>
+          </div>
+          <div className="w-full md:w-1/2 flex flex-col justify-center items-start gap-8">
+            <h3 className="text-heading3-bold text-center md:text-left w-full">Praktikaga seotud juhendid ja materjalid</h3>
+            <p className="text-large-medium text-center md:text-left">IT-akadeemia praktikatega seotud juhendid ja materjalid leiad siit.</p>
+            <div className="grid grid-cols-2 w-full gap-4">
+              {praktikaLinks.map((link) => (
+                <a href={link.link} className='flex justify-start gap-2 items-center'>
+                  <img src={documentIcon} className="w-5"/>
+                  <p className="text-itkool text-base-bold hover:underline">{link.text}</p>
+                </a>
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
