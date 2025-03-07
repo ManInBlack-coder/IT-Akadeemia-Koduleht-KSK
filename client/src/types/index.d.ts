0c9fd1d38af4971a8dce3806c0fc6133b27d0398
gitTanzj <kalle.riit@gmail.com>
2025-03-07 11:14:40 +0200

    fix: room filter and color coded classes on timetable

--- client/src/types/index.d.ts
+++ client/src/types/index.d.ts
@@ -39,16 +39,4 @@ type ScheduleItem = {
 type ScheduleType = {
   nadal: string;
   tunnid: { [key: string]: ScheduleItem[] };
-  ajad: {
-    "1": string;
-    "2": string;
-    "3": string;
-    "4": string;
-    "5": string;
-    "6": string;
-    "7": string;
-    "8": string;
-    "9": string;
-    "soomine": string;
-  };
 };
