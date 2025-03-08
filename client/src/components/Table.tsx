4fa7b95cfb3a9a5cb0d1c35caa52b9d8fc79bc10
gitTanzj <kalle.riit@gmail.com>
2025-03-08 14:09:33 +0200

    chore: finish rebsae

--- client/src/components/Table.tsx
+++ client/src/components/Table.tsx
@@ -47,10 +47,13 @@ export const Table: React.FC<TableProps> = ({ week, setWeek, type, data, title }
   };
 
 <<<<<<< HEAD
+<<<<<<< HEAD
   // Funktsioon, mis geneerib värvi, tiitli hashist lähtudes
 =======
   // Function to generate a color based on the item's title
 >>>>>>> dee4c12 (fix: room filter and color coded classes on timetable)
+=======
+>>>>>>> 9cc4d2c (chore: merge success pt2)
   const generateColor = (title: string): string => {
     if(title.length > 0){
       const hash = title.split('').reduce((acc, char) => char.charCodeAt(0) + acc, 0);
