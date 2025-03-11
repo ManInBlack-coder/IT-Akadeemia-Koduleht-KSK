5780928573a0775923095236277e4019706be169
Tuvi9 <kaspupavel@gmail.com>
2025-03-11 16:14:10 +0200

    feat: desktop avaleht styling.

--- client/src/pages/Avaleht/Avaleht.tsx
+++ client/src/pages/Avaleht/Avaleht.tsx
@@ -25,13 +25,6 @@ const eventsData: {
       date: "12.12.2024",
       time: "12.00 – 18.00",
       image: example2
-    },
-    {
-      title: "IT Karjääripäev",
-      location: "Peahoone",
-      date: "12.12.2024",
-      time: "10.00 – 16.00",
-      image: example2
     }
   ],
   peagiTulekul: [
@@ -64,6 +57,13 @@ const eventsData: {
       date: "15.01.2025 - 15.03.2025",
       time: "18.00 – 20.00",
       image: example2
+    },
+    {
+      title: "IT Karjääripäev",
+      location: "Peahoone",
+      date: "12.12.2024",
+      time: "10.00 – 16.00",
+      image: example2
     }
   ]
 };
@@ -82,79 +82,93 @@ const Avaleht = () => {
           </div>
       </div>
       <div className='bg-itkool flex w-full justify-center min-h-80 px-4'>
-        <div className="z-1 flex text-white font-semibold justify-center items-center flex-col text-center gap-8 md:gap-12 py-8 w-full max-w-4xl">
+        <div className="z-1 flex text-white font-semibold justify-center items-center flex-col text-center gap-8 md:gap-12 py-8 w-full max-w-5xl">
           <div className='relative w-full'>
             <h1 className="text-heading3-bold md:text-heading1-bold text-2xl md:text-[40px] text-center">
-              Miks valida IT<br/>Akadeemia?
+              Miks valida IT <br className='block md:hidden'/>Akadeemia?
             </h1>
             <img 
-              className='w-8 h-8 absolute' 
-              style={{ bottom: '0.25rem', left: 'calc(50% + 5rem)' }} 
+              className='w-8 h-8 absolute left-[calc(50%+5rem)] bottom-1 lg:left-[calc(50%+14rem)] lg:bottom-1.5'
               src={technologist} 
               alt="technologist" 
             />
           </div>
-          <h2 className="text-large-medium text-center">Pakume <span className="text-large-bold">mitmekesiseid</span><br/> õppimisvõimalusi ja <span className="text-large-bold">tihedat koostööd</span><br/> <span className="text-large-bold">ettevõtetega</span>, et aidata sul saavutada<br/> oma eesmärgid.</h2>
+          <h2 className="text-large-medium lg:text-heading5-bold text-center ">Pakume <span className="text-large-bold lg:text-heading5-bold">mitmekesiseid</span><br className='block lg:hidden'/> õppimisvõimalusi ja <span className="text-large-bold lg:text-heading5-bold">tihedat koostööd</span><br className='block lg:hidden'/> <span className="text-large-bold lg:text-heading5-bold">ettevõtetega</span>, et aidata sul saavutada<br className='block lg:hidden'/> oma eesmärgid.</h2>
         </div>
       </div>
-      <div className='flex flex-col items-center gap-y-[24px]'>
-        <div className='bg-vocogray flex w-5/6 flex-col min-h-80 mt-[46px]'>
-          <div className='flex flex-col gap-y-[37px] items-center'>
-            <div className={`example flex w-full min-h-80`}></div>
-            <div className='flex text-heading6-bold text-center w-5/6'>Võimalusterohke õppimine IT ja ärivaldkonnas</div>
+      <div className='flex flex-col lg:w-full items-center gap-y-[24px] lg:gap-y-[48px]'>
+        {/* First */}
+        <div className='bg-vocogray flex w-5/6 lg:w-4/6 flex-col lg:flex-row min-h-80 lg:min-h-[500px] mt-[46px]'>
+          <div className='flex flex-col gap-y-[37px] items-center lg:items-start lg:justify-center lg:w-1/2 lg:pl-16 lg:py-8'>
+            <div className='lg:hidden flex w-full min-h-80 example'></div>
+            <div className='flex text-heading6-bold lg:text-heading3-bold text-center lg:text-left w-5/6 lg:w-full'>Võimalusterohke õppimine IT ja ärivaldkonnas</div>
+            <div className='flex justify-center lg:justify-start mt-[24px] mb-[24px] lg:mt-8 lg:mb-0'>
+              <Link to="/erialad">
+                <button className='border bg-black px-[44px] py-[14px]'>
+                  <h2 className="text-base-medium text-white">Leia Eriala</h2>
+                </button>
+              </Link>
+            </div>
           </div>
-          <div className='flex justify-center mt-[24px] mb-[24px]'>
-            <Link to="/erialad">
-              <button className='border bg-black px-[44px] py-[14px]'>
-                    <h2 className="text-base-medium text-white">Leia Eriala</h2>
-              </button>
-            </Link>
-          </div>
-        </div>
-        <div className='bg-vocogray flex w-5/6 flex-col min-h-80'>
-          <div className='flex flex-col gap-y-[37px] items-center'>
-            <div className={`example flex w-full min-h-80`}></div>
-            <div className='flex text-heading6-bold text-center w-5/6'>Võimalusterohke õppimine IT ja ärivaldkonnas</div>
-          </div>
-          <div className='flex justify-center mt-[24px] mb-[24px]'>
-            <Link to="/erialad">
-              <button className='border bg-black px-[44px] py-[14px]'>
-                    <h2 className="text-base-medium text-white">Leia Eriala</h2>
-              </button>
-            </Link>
+          <div className='hidden lg:flex lg:w-1/2 lg:min-h-full'>
+            <div className='w-full h-full bg-cover bg-center example'></div>
           </div>
         </div>
-        <div className='bg-vocogray flex w-5/6 flex-col min-h-80'>
-          <div className='flex flex-col gap-y-[37px] items-center'>
-            <div className={`example flex w-full min-h-80`}></div>
-            <div className='flex text-heading6-bold text-center w-5/6'>Võimalusterohke õppimine IT ja ärivaldkonnas</div>
+        {/* Second */}
+        <div className='bg-vocogray flex w-5/6 lg:w-4/6 flex-col lg:flex-row min-h-80 lg:min-h-[500px]'>
+          <div className='hidden lg:flex lg:w-1/2 lg:min-h-full order-1 lg:order-none'>
+            <div className='w-full h-full bg-cover bg-center example'></div>
           </div>
-          <div className='flex justify-center mt-[24px] mb-[24px]'>
-            <Link to="/erialad">
-              <button className='border bg-black px-[44px] py-[14px]'>
-                    <h2 className="text-base-medium text-white">Leia Eriala</h2>
-              </button>
-            </Link>
+          <div className='flex flex-col gap-y-[37px] items-center lg:items-start lg:justify-center lg:w-1/2 lg:pl-16 lg:py-8'>
+            <div className='lg:hidden flex w-full min-h-80 example'></div>
+            <div className='flex text-heading6-bold lg:text-heading3-bold text-center lg:text-left w-5/6 lg:w-full'>Võimalusterohke õppimine IT ja ärivaldkonnas</div>
+            <div className='flex justify-center lg:justify-start mt-[24px] mb-[24px] lg:mt-8 lg:mb-0'>
+              <Link to="/erialad">
+                <button className='border bg-black px-[44px] py-[14px]'>
+                  <h2 className="text-base-medium text-white">Leia Eriala</h2>
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
+        {/* Third */}
+        <div className='bg-vocogray flex w-5/6 lg:w-4/6 flex-col lg:flex-row min-h-80 lg:min-h-[500px]'>
+          <div className='flex flex-col gap-y-[37px] items-center lg:items-start lg:justify-center lg:w-1/2 lg:pl-16 lg:py-8'>
+            <div className='lg:hidden flex w-full min-h-80 example'></div>
+            <div className='flex text-heading6-bold lg:text-heading3-bold text-center lg:text-left w-5/6 lg:w-full'>Võimalusterohke õppimine IT ja ärivaldkonnas</div>
+            <div className='flex justify-center lg:justify-start mt-[24px] mb-[24px] lg:mt-8 lg:mb-0'>
+              <Link to="/erialad">
+                <button className='border bg-black px-[44px] py-[14px]'>
+                  <h2 className="text-base-medium text-white">Leia Eriala</h2>
+                </button>
+              </Link>
+            </div>
+          </div>
+          <div className='hidden lg:flex lg:w-1/2 lg:min-h-full'>
+            <div className='w-full h-full bg-cover bg-center example'></div>
           </div>
         </div>
       </div>
-      <div className='bg-vocogray flex flex-col w-full  min-h-screen px-4 mt-[79px] pb-[98px]'>
+      <div className='bg-vocogray flex flex-col w-full min-h-screen px-4 mt-[79px] pb-[98px]'>
         <div className='flex flex-col text-center gap-y-[28px] mt-[68px]'>
-          <div className='text-heading3-bold '>SÜNDMUSED VOCO-s</div>
-          <div className='text-large-medium'>Hoia end kursis VOCO-s toimuvate põnevate üritustega! </div>
+          <div className='text-heading3-bold'>SÜNDMUSED VOCO-s</div>
+          <div className='text-large-medium'>Hoia end kursis VOCO-s toimuvate põnevate üritustega!</div>
         </div>
-        {/* Events sections */}
-        <div className='flex flex-col gap-y-[60px]'>
+        
+        {/* Events sections - Mobile view is stacked, Desktop view is 3 columns */}
+        <div className='flex flex-col lg:flex-row lg:justify-between lg:gap-x-8 max-w-7xl mx-auto w-full mt-[46px] lg:mt-[94px]'>
           {/* Täna */}
-          <div className='mt-[46px]'>
-            <p className='text-heading6-bold text-itkool'>Täna</p>
+          <div className='mb-[60px] lg:mb-0 lg:w-1/3'>
+            <div className='lg:flex lg:w-full lg:justify-center'>
+              <p className='text-heading6-bold text-itkool'>Täna</p>
+            </div>
             {eventsData.tana.map((event, index) => (
               <div key={`tana-${index}`} className='flex flex-row mt-[28px]'>
-                <img src={event.image} alt={event.title} />
+                <img src={event.image} alt={event.title} className="w-24 h-24 object-cover" />
                 <div className='flex flex-col ml-[17px] gap-y-[12px]'>
                   <div className='text-large-bold'>{event.title}</div>
-                  <div className='text-small-regular w-3/4'>
-                    {event.location} {event.date}<br/> kl {event.time}
+                  <div className='text-small-regular'>
+                    {event.location}<br/>{event.date}<br/>kl {event.time}
                   </div>
                 </div>
               </div>
@@ -162,15 +176,17 @@ const Avaleht = () => {
           </div>
           
           {/* Peagi tulekul */}
-          <div>
-            <p className='text-heading6-bold text-itkool'>Peagi tulekul</p>
+          <div className='mb-[60px] lg:mb-0 lg:w-1/3'>
+            <div className='lg:flex lg:w-full lg:justify-center'>
+              <p className='text-heading6-bold text-itkool'>Peagi tulekul</p>
+            </div>
             {eventsData.peagiTulekul.map((event, index) => (
               <div key={`peagi-${index}`} className='flex flex-row mt-[28px]'>
-                <img src={event.image} alt={event.title} />
+                <img src={event.image} alt={event.title} className="w-24 h-24 object-cover" />
                 <div className='flex flex-col ml-[17px] gap-y-[12px]'>
                   <div className='text-large-bold'>{event.title}</div>
-                  <div className='text-small-regular w-3/4'>
-                    {event.location} {event.date}<br/> kl {event.time}
+                  <div className='text-small-regular'>
+                    {event.location}<br/>{event.date}<br/>kl {event.time}
                   </div>
                 </div>
               </div>
@@ -178,15 +194,17 @@ const Avaleht = () => {
           </div>
           
           {/* Pikkaajalised */}
-          <div>
-            <p className='text-heading6-bold text-itkool'>Pikkaajalised</p>
+          <div className='lg:w-1/3'>
+            <div className='lg:flex lg:w-full lg:justify-center'>
+              <p className='text-heading6-bold text-itkool'>Pikkaajalised</p>
+            </div>
             {eventsData.pikkaajalised.map((event, index) => (
               <div key={`pikk-${index}`} className='flex flex-row mt-[28px]'>
-                <img src={event.image} alt={event.title} />
+                <img src={event.image} alt={event.title} className="w-24 h-24 object-cover" />
                 <div className='flex flex-col ml-[17px] gap-y-[12px]'>
                   <div className='text-large-bold'>{event.title}</div>
-                  <div className='text-small-regular w-3/4'>
-                    {event.location} {event.date}<br/> kl {event.time}
+                  <div className='text-small-regular'>
+                    {event.location}<br/>{event.date}<br/>kl {event.time}
                   </div>
                 </div>
               </div>
