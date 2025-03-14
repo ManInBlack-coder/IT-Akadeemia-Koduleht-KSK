db808bc73455f079a569e5c146e2ed649d1ce115
Aleksander P <77441538+ManInBlack-coder@users.noreply.github.com>
2025-03-14 08:43:14 +0200

    fixed buttons text position

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -10,7 +10,7 @@ interface ButtonProps {
 export const Button: React.FC<ButtonProps> = ({ buttonText, color, onClick }) => {
   return (
     <div className={`${color === 'black' ? 'button-hover-black' : 'button-hover-blue'} p-4 flex justify-center items-center ${color === 'black' ? 'bg-black' : 'bg-itkool'} hover:cursor-pointer`} onClick={onClick}>
-        <p className="text-xl text-large-bold px-8 text-white">{buttonText}</p>
+        <p className="text-xl text-center text-large-bold px-8 text-white">{buttonText}</p>
     </div>
   )
 }
